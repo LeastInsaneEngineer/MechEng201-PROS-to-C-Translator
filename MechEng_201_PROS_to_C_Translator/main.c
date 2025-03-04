3bc733ddf3cc2f0c4c13c8f3146f9d2535bf02cb
LeastInsaneEngineer <alifakmaloni@gmail.com>
2025-03-05 10:50:21 +1300

    commited the correct files

---
+++ MechEng_201_PROS_to_C_Translator/main.c
@@ -0,0 +1,22 @@
+// PROS to C Translator for UoA Mechanical / Mechatronical Engineering 201 Vex Project
+// Created by: Khalif Akmaloni
+// This translator is so you can emulate, compile and run functions for testing outside of MechEng201 lab sessions.
+
+#include "main.h"
+
+//initialise your functions below this comment
+
+// Initialise your functions above this comment
+
+// Run your program in main.
+void main()
+{
+	// This array simulates your encoders. It must be passed into every function you have.
+	// As arrays are implicitly pointers, by passing this array into every function, the value in the encoder array will be consistent everywhere.
+	// This array is updated by the motorPower() function and can then be used for closedloop controls	
+	int encoder[] = { 0,0,0 };
+
+
+}
+
+// Define your functions below this comment.
